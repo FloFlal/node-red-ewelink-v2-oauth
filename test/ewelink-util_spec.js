88e2889f5baf8995d16f277489d671fda425154e
FloFlal <florian.chambon.zz@gmail.com>
2026-01-06 14:59:10 +0100

    fix test to use really received variables + add new test case for automatic at expiration date

--- test/ewelink-util_spec.js
+++ test/ewelink-util_spec.js
@@ -70,8 +70,8 @@ describe('eWeLink util tests', () => {
 		const userStub = sinon.stub(user, 'refreshToken').callsFake(() => Promise.resolve({
 			error: 0,
 			data: {
-				accessToken: 'newAt',
-				refreshToken: 'newRt',
+				at: 'newAt',
+				rt: 'newRt',
 				atExpiredTime: (new Date()).getTime() + (24*60*60*1000*29),
 				rtExpiredTime: (new Date()).getTime() + (24*60*60*1000*59),
 			},
@@ -131,6 +131,86 @@ describe('eWeLink util tests', () => {
 				if (client.region !== 'eu') throw new Error("The region property is not OK");
 				if (client.at !== 'newAt') throw new Error("The at property is not OK");
 				if (client.rt !== 'newRt') throw new Error("The rt property is not OK");
+
+				// release all stubs
+				userStub.restore();
+				clientStub.restore();
+				utilStub.restore();
+
+				done();
+
+				return Promise.resolve({error: 0, data: 'test'})
+			});
+		});
+	});
+
+	it('Should put default values on token expiration date if not provided', done => {
+		// override the user.refreshToken method of the client
+		const user = { refreshToken: () => { }};
+		const userStub = sinon.stub(user, 'refreshToken').callsFake(() => Promise.resolve({
+			error: 0,
+			data: {
+				at: 'newAt',
+				rt: 'newRt'
+			},
+			msg: null
+		}));
+
+		// override the home value of the clinet
+  		const client = {
+  			user: {},
+  			displayName: 'displayName',
+	    	appId: 'appId',
+	    	appSecret: 'appSecret',
+	    	region: 'eu',
+	    	atExpiredTime: (new Date()).getTime() - (24*60*60*1000),
+	    	rtExpiredTime: undefined,
+	    	at: 'atToken',
+	    	rt: 'rtToken'
+  		};
+  		const clientStub = sinon.stub(client, "user").value(user);
+
+  		// override the creation of the client
+		const utilStub = sinon.stub(util, 'getBasicClient').callsFake(() => client);
+
+		const flow = [
+	      { id: 'n2', type: 'ewelink-auth' }
+	    ];
+
+	    const cred = {
+	    	n2: {
+	    		displayName: 'displayName',
+	    		appId: 'appId',
+	    		appSecret: 'appSecret',
+	    		region: 'eu',
+	    		atExpiredTime: (new Date()).getTime() - (24*60*60*1000),
+	    		at: 'atToken',
+	    		rt: 'rtToken'
+	    	}
+	    }
+
+	    const RED = {
+	    	nodes: {
+	    		addCredentials: function (id, credentials) {
+	    			return;
+	    		}
+	    	}
+	    }
+
+	    helper.load(authNode, flow, cred, () => {
+	    	const n2 = helper.getNode('n2');
+
+			util.handleEwelinkResponse(RED, n2, {payload: 'test'}, () => {}, client => {
+				if(Object.keys(client).length === 0){
+					throw new Error("Expected an object not empty got: " + JSON.stringify(client));
+				}
+				
+				if (client.appId !== 'appId') throw new Error("The appId property is not OK");
+				if (client.appSecret !== 'appSecret') throw new Error("The appSecret property is not OK");
+				if (client.region !== 'eu') throw new Error("The region property is not OK");
+				if (client.at !== 'newAt') throw new Error("The at property is not OK");
+				if (client.rt !== 'newRt') throw new Error("The rt property is not OK");
+				if (client.atExpiredTime === undefined || client.atExpiredTime <= (new Date()).getTime() - (24*60*60*1000)) throw new Error("The atExpiredTime property is not OK");
 
 				// release all stubs
 				userStub.restore();
