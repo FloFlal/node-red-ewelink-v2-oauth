05775f5d59e8a3e0a7ce5c002b2fe97f8040805d
Florian Chambon <florian.chambon_ext@michelin.com>
2026-01-16 21:44:14 +0100

    add end of 1st version of websocket + tests

--- src/ewelink-websocket/ewelink-websocket.js
+++ src/ewelink-websocket/ewelink-websocket.js
@@ -10,8 +10,6 @@ module.exports = function(RED) {
 
         var node = this;
 
-        console.log('[' + new Date() + '] - Initializing eWeLink Websocket Node :' + JSON.stringify(config));
-        console.log('[' + new Date() + '] - Initializing eWeLink the client :' + JSON.stringify(node.auth.getWebsocketClient()));
         
         var ws = node.auth.getWebsocketClient().Connect.create(
             {
@@ -23,7 +21,6 @@ module.exports = function(RED) {
             (_ws) => {
                 ws = _ws;
                 node.log('[' + new Date() + '] - Connected to eWeLink WS: ' + ws.url);
-                console.log('[' + new Date() + '] - Connected to eWeLink WS: ' + ws.url);
                 node.status({
                     fill: 'yellow',
                     shape: 'ring',
@@ -32,7 +29,6 @@ module.exports = function(RED) {
             },
             () => {
                 node.log('[' + new Date() + '] - Disconnected from eWeLink WS');
-                console.log('[' + new Date() + '] - Disconnected from eWeLink WS');
                 node.status({
                     fill: 'red',
                     shape: 'ring',
@@ -41,7 +37,6 @@ module.exports = function(RED) {
             },
             (error) => {
                 node.error('[' + new Date() + '] - eWeLink WS connection error: ', error);
-                console.error('[' + new Date() + '] - eWeLink WS connection error: ', error);
                 node.status({
                     fill: 'red',
                     shape: 'dot',
@@ -51,10 +46,9 @@ module.exports = function(RED) {
             (_ws, message) => {
                 data = message.data;
                 node.log('[' + new Date() + '] - eWeLink WS message: ', JSON.stringify(data));
-                console.log('[' + new Date() + '] - eWeLink WS message: ', JSON.stringify(data));
 
                 // Handle the handcheck response that is indicating that the connection is established
-                if (message.data.toString()[0] === "{" && JSON.parse(message.data.toString())?.config) {
+                if (data !== undefined && JSON.stringify(data)[0] === "{" && data.config !== undefined) {
                     node.status({
                         fill: 'green',
                         shape: 'dot',
@@ -66,29 +60,24 @@ module.exports = function(RED) {
                 // Handle other messages
                 if (
                     data                                                    // Check that we have a message
-                    && data[0] === '{'                                      // Check that it is not a technical message (ping/pong)
+                    && JSON.stringify(data)[0] === '{'                      // Check that it is not a technical message (ping/pong)
                     && (
                         node.deviceId === ''                                // if there is no deviceId, then send all message
-                        || JSON.parse(data).deviceid === node.deviceId      // if there is a deviceId, send message only if this deviceId correspond to the deviceId of the message
+                        || data.deviceid === node.deviceId                  // if there is a deviceId, send message only if this deviceId correspond to the deviceId of the message
                     )
                 ) {
-                    node.send({payload: data});                             // send the message
+                    node.send({payload: data});                              // send the message
                 }
                 return false;
             }
         );
 
-        console.log('[' + new Date() + '] - eWeLink Websocket Node WS initialized');
-
         node.on('close', function() {
             if (ws) {
                 ws.close();
                 node.log('[' + new Date() + '] - eWeLink WS connection closed');
-                console.log('[' + new Date() + '] - eWeLink WS connection closed');
             }
         });
-        
-        console.log('[' + new Date() + '] - eWeLink Websocket Node end of node configuration');
     }
     RED.nodes.registerType("ewelink-websocket",EwelinkWebsocket);
 }
